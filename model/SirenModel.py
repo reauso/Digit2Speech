b46a7a403d1386c883127678903881777d173667
reauso <r.ebertowski@gmx.de>
2023-03-22 16:05:33 +0100

    added Positional Encoding Option to SIREN Network

--- model/SirenModel.py
+++ model/SirenModel.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from model.ModulationLayer import FiLM, MappingNetwork
+from model.PositionalEncoding import HarmonicEmbedding
 
 
 class MappingType(Enum):
@@ -52,18 +53,36 @@ class SineLayerWithFilm(nn.Module):
 
 
 class SirenModelWithFiLM(nn.Module):
-    def __init__(self, in_features, out_features, hidden_features, hidden_layers,
-                 mod_in_features, mod_features=256, mod_hidden_layers=3, first_omega_0=30, hidden_omega_0=30.,
-                 modulation_type: MappingType = MappingType.Mult_Networks_One_Dimension_For_Each_Layer
-                 ):
+    def __init__(
+            self,
+            in_features,
+            out_features,
+            hidden_features,
+            hidden_layers,
+            mod_in_features,
+            mod_features=256,
+            mod_hidden_layers=3,
+            first_omega_0=30,
+            hidden_omega_0=30.,
+            modulation_type: MappingType = MappingType.Mult_Networks_One_Dimension_For_Each_Layer,
+            use_harmonic_embedding=True,
+            num_harmonic_embeddings=30,
+            use_mod_harmonic_embedding=True,
+            num_mod_harmonic_embedding=4,
+    ):
         super().__init__()
         self.hidden_features = hidden_features
         self.mapping_type = modulation_type
+        self.use_harmonic_embedding = use_harmonic_embedding
 
         self.sine_layers = []
         self.final_layers = []
 
+        # harmonic embedding
+        self.harmonic_embedding = HarmonicEmbedding(n_harmonic_functions=num_harmonic_embeddings)
+
         # sine layers
+        in_features = in_features * num_harmonic_embeddings * 2 if self.use_harmonic_embedding else in_features
         self.sine_layers.append(SineLayerWithFilm(in_features, hidden_features,
                                                   is_first=True, omega_0=first_omega_0))
 
@@ -71,14 +90,13 @@ class SirenModelWithFiLM(nn.Module):
             self.sine_layers.append(SineLayerWithFilm(hidden_features, hidden_features,
                                                       is_first=False, omega_0=hidden_omega_0))
 
+        # final layers
         final_linear = nn.Linear(hidden_features, out_features)
         with torch.no_grad():
             final_linear.weight.uniform_(-np.sqrt(6 / hidden_features) / hidden_omega_0,
                                          np.sqrt(6 / hidden_features) / hidden_omega_0)
 
-        # final layers
         self.final_layers.append(final_linear)
-        # self.final_layers.append(torch.nn.Tanh())
 
         # as sequential
         self.sine_layers = nn.Sequential(*self.sine_layers)
@@ -86,15 +104,24 @@ class SirenModelWithFiLM(nn.Module):
 
         # modulation network(s)
         self.modulation_networks = self._get_modulation(mod_in_features, hidden_features,
-                                                        hidden_layers, mod_features, mod_hidden_layers)
+                                                        hidden_layers, mod_features, mod_hidden_layers,
+                                                        use_mod_harmonic_embedding, num_mod_harmonic_embedding)
         self.modulation_dimension_mapping = {
             MappingType.One_Network_One_Dimension_For_All_Layers: 1,
             MappingType.One_Network_Mult_Dimension_For_Each_Layer: hidden_layers + 1,
             MappingType.Mult_Networks_One_Dimension_For_Each_Layer: hidden_layers + 1,
         }
 
-    def _get_modulation(self, mod_in_features, hidden_features, hidden_layers, mod_features,
-                        mod_hidden_layers):
+    def _get_modulation(
+            self,
+            mod_in_features,
+            hidden_features,
+            hidden_layers,
+            mod_features,
+            mod_hidden_layers,
+            use_mod_harmonic_embedding,
+            num_mod_harmonic_embedding,
+    ):
         if self.mapping_type is MappingType.One_Network_One_Dimension_For_All_Layers:
             modulation_network = torch.nn.ModuleList([
                 MappingNetwork(
@@ -103,6 +130,8 @@ class SirenModelWithFiLM(nn.Module):
                     num_dimensions=1,
                     num_features=mod_features,
                     hidden_layers=mod_hidden_layers,
+                    use_harmonic_embedding=use_mod_harmonic_embedding,
+                    num_harmonic_functions=num_mod_harmonic_embedding,
                 )])
         elif self.mapping_type is MappingType.One_Network_Mult_Dimension_For_Each_Layer:
             modulation_network = torch.nn.ModuleList([
@@ -112,6 +141,8 @@ class SirenModelWithFiLM(nn.Module):
                     num_dimensions=hidden_layers + 1,
                     num_features=mod_features,
                     hidden_layers=mod_hidden_layers,
+                    use_harmonic_embedding=use_mod_harmonic_embedding,
+                    num_harmonic_functions=num_mod_harmonic_embedding,
                 )])
         elif self.mapping_type is MappingType.Mult_Networks_One_Dimension_For_Each_Layer:
             modulation_network = torch.nn.ModuleList([
@@ -121,6 +152,8 @@ class SirenModelWithFiLM(nn.Module):
                     num_dimensions=1,
                     num_features=mod_features,
                     hidden_layers=mod_hidden_layers,
+                    use_harmonic_embedding=use_mod_harmonic_embedding,
+                    num_harmonic_functions=num_mod_harmonic_embedding,
                 ) for _ in range(hidden_layers + 1)
             ])
         else:
@@ -129,13 +162,20 @@ class SirenModelWithFiLM(nn.Module):
         return modulation_network
 
     def forward(self, x, modulation_input):
+        # harmonic embedding
+        if self.use_harmonic_embedding:
+            x = self.harmonic_embedding(x)
+
+        # modulation
         scale, shift = self.calculate_scale_and_shift(modulation_input)
 
+        # sine layers
         mod_dimensions = self.modulation_dimension_mapping[self.mapping_type]
         for i, layer in enumerate(self.sine_layers):
             mod_index = min(i, mod_dimensions - 1)
             x = layer(x, scale[mod_index], shift[mod_index])
 
+        # final layers
         x = self.final_layers(x)
 
         return torch.sin(x)
